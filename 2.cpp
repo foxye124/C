6d3ad4b66d126d6ba2bcf2cb1076dc9f27f658a2
foxyer124 <foxyer124@gmail.com>
2026-08-07 03:09:25 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "choose 0 for numbers to stop" "\n";
+    while (true)
+    {
+    
+        long long number1;
+        long long number2;
+
+        std::cout << "choose a number: ";
+        std::cin >> number1;
+
+        std::cout << "choose a second number: ";
+        std::cin >> number2;
+
+        if (number1 == 0 && number2 ==0)
+        {
+            return 0;
+        }
+        
+        long long sum1 = number1 + number2;
+        long long sum2 = number1 - number2;
+        long long sum3 = number2 - number1;
+
+        std::cout << "you choose " << number1 << " and " << number2 << " it's sum is: " << sum1 << "\n";
+
+        if (number1 > number2)
+        {
+            std::cout << "And your first chosen number is higher by " << sum2 << "\n";
+        }
+        else if (number1 < number2)
+        {
+            std::cout << "And your second chosen number is higher by " << sum3 << "\n";
+        }
+        else
+        {
+            std::cout << "And your number are same." "\n";
+        }
+    
+    }
+}
